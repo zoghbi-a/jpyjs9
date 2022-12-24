1b9c717990475d32d34eb277d1014acd8a643bb2
Abdu Zoghbi <a.zoghbi@nasa.gov>
2022-12-24 15:47:47 -0500

    configured basic package setup

--- jpyjs9/__about__.py
+++
@@ -1,4 +0,0 @@
-# SPDX-FileCopyrightText: 2022-present Abdu Zoghbi <a.zoghbi@nasa.gov>
-#
-# SPDX-License-Identifier: MIT
-__version__ = '0.0.1'
--- jpyjs9/__init__.py
+++ jpyjs9/__init__.py
@@ -1,3 +1,5 @@
 # SPDX-FileCopyrightText: 2022-present Abdu Zoghbi <a.zoghbi@nasa.gov>
 #
 # SPDX-License-Identifier: MIT
+
+__version__ = '0.1'
